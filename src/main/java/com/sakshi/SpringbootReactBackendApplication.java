502777ef387d38ad64b740977069be6f7892e894
sakshi goyal <sakshigoyal7351@gmail.com>
2023-08-30 18:23:53 +0530

    first commit

---
+++ src/main/java/com/sakshi/SpringbootReactBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sakshi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReactBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootReactBackendApplication.class, args);
+	}
+
+}
